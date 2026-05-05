2b04627c2212a2c1610cab69fdc086e115252dc1
KierPalin <45743174+KierPalin@users.noreply.github.com>
2026-05-05 16:35:15 +0100

    ExampleMicroGUIScene additions

--- exampleMicroGUIScene.ts
+++ exampleMicroGUIScene.ts
@@ -1,34 +1,41 @@
+// This is an example of making a scene with microgui.
+// microgui is a nascent extension that endeavours to make it easier to develop complex scenes.
+// It provides components that you can easily manipulate to make more complex scenes.
+// If you have problems with microgui, please file an issue or reach out. https://github.com/microbit-apps/MicroGUI/issues
 namespace example_project {
   import AppInterface = user_interface_base.AppInterface
-  import Screen = user_interface_base.Screen
+
+  // microgui enables scenes to be made up of hetrogenous components:
+  import GUIComponentScene = microgui.GUIComponentScene
+  import GUIComponentAbstract = microgui.GUIComponentAbstract
   import GUIComponentAlignment = microgui.GUIComponentAlignment
   import TextBox = microgui.TextBox
-  import GUIComponentScene = microgui.GUIComponentScene
+  // Other microgui components:
   // import TextButtonCollection = microgui.TextButtonCollection
   // import TextButton = microgui.TextButton
-  import GUIComponentAbstract = microgui.GUIComponentAbstract
 
   export class ExampleMicroGUIScene extends GUIComponentScene {
     constructor(app: AppInterface) {
-      super({app, colour: 6}) // 6 = light blue w/ default palette
+      super({ app, colour: 6 }) // 6 = light blue w/ default palette
 
+      // microgui components are simple blocks with lots of optional arguments
       const simpleTextComponent: GUIComponentAbstract = new TextBox({
         alignment: GUIComponentAlignment.BOT,
         isActive: false,
         title: "Title Text :)", // optional arg
         text: ["Press micro:bit A btn"], // optional arg
-        colour: 6, // optional arg
+        colour: 3, // optional arg
         xScaling: 1.7, // optional arg
       });
 
       // Cast the object to access TextBox-specific properties
       (simpleTextComponent as TextBox).title = "hi";
 
+      // Manipulating a component after it's been created:
       let count = 0;
       input.onButtonPressed(1, function () {
         count++;
-        simpleTextComponent.clearContext()
-        simpleTextComponent.addContext(["" + count])
+        (simpleTextComponent as TextBox).text = ["" + count];
       })
 
       this.components = [simpleTextComponent]
