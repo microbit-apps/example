2ceac0b9670f2c49e688584421d4fc5843d665ee
KierPalin <45743174+KierPalin@users.noreply.github.com>
2026-05-11 13:59:58 +0100

    custom.ts -> simpleCursorScene.ts

---
+++ simpleCursorScene.ts
@@ -0,0 +1,53 @@
+namespace example_project {
+  import Screen = user_interface_base.Screen
+  import CursorScene = user_interface_base.CursorScene
+  import Button = user_interface_base.Button
+  import ButtonStyles = user_interface_base.ButtonStyles
+  import AppInterface = user_interface_base.AppInterface
+
+  export class SimpleCursorScene extends CursorScene {
+    constructor(app: AppInterface) {
+      super(app)
+    }
+
+    /* override */
+    startup() {
+      super.startup()
+
+      control.onEvent(
+        ControllerButtonEvent.Pressed,
+        controller.B.id,
+        () => {
+          this.app.popScene()
+        }
+      )
+
+      this.navigator.setBtns([[
+        new Button({
+          parent: null,
+          style: ButtonStyles.Transparent,
+          icon: "accelerometer",
+          ariaId: "Enter ExampleScene",
+          x: 0,
+          y: 0,
+          onClick: () => {
+            this.app.pushScene(new ExampleScene(this.app))
+          },
+        })
+      ]])
+    }
+
+    draw() {
+      Screen.fillRect(
+        Screen.LEFT_EDGE,
+        Screen.TOP_EDGE,
+        Screen.WIDTH,
+        Screen.HEIGHT,
+        12 // purple in default palette
+      )
+
+      this.navigator.drawComponents();
+      super.draw()
+    }
+  }
+}
